a62552a5d51772dc82b0a842422667492af98fe3
kevinreber <kevinreber1@gmail.com>
2020-07-13 12:29:53 -0700

    front end set up

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jobly</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
