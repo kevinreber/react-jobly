bd9d8b4d0dd3a065e70983a89e169739d39d5235
kevinreber <kevinreber1@gmail.com>
2020-07-14 20:34:58 -0700

    add: Company and JobCard component

--- frontend/src/Components/Company.jsx
+++ frontend/src/Components/Company.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Redirect, useParams } from "react-router-dom";
+import React, { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import JobCard from './JobCard';
+import JoblyApi from '../JoblyApi';
 
 function Company(){
+    const [company, setCompany] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    /** Will store `jobList` of company's jobs */
+    let jobList;
+
+    /** Get handle */
+    const { handle } = useParams();
+
+    /** Get company data */
+    useEffect(() =>{
+        const getData = async () => {
+            const results = await JoblyApi.getCompany(handle);
+            setCompany(results);
+            setIsLoading(false);
+        }
+        getData();
+    }, []);
+
+    if (isLoading) {
+        return <h3>Loading...</h3>;
+    }
+
+    /** Build `jobList` when loading company data is finished */
+    if(!isLoading){
+        jobList = company.jobs.map(job => (
+            <JobCard job={job} />
+        ));
+    }
+
     return(
         <div className="Company">
-            <h1>Company</h1>
+            <h1 className="Company-Title p-3">{company.name}</h1>
+            <div className="JobList container d-flex flex-wrap">
+                {jobList}
+            </div>
         </div>
     )
 }
