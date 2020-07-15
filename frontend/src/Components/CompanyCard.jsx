e2a5afbf9b55ef6df7ed19b9b75995a9f487df8b
kevinreber <kevinreber1@gmail.com>
2020-07-14 21:47:56 -0700

    style: re-styled CompanyCard component

--- frontend/src/Components/CompanyCard.jsx
+++ frontend/src/Components/CompanyCard.jsx
@@ -6,12 +6,18 @@ function CompanyCard({company}){
     const defaultLogo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRWpMt1I4YD-NiUmlnM-PPrUL-6BR84_HTRBw&usqp=CAU';
     const companyLogo = company.logo_url ? company.logo_url : defaultLogo;
     return(
-        <div id={company.handle} className="CompanyCard card w-75">
+        <div id={company.handle} className="CompanyCard card w-75 mr-auto ml-auto mb-3">
         <Link to={`/companies/${company.handle}`} >
-            <img src={companyLogo} alt={company.name} className="card-img-top rounded-circle border border-dark" />
+        <div id={company.handle} className="d-inline-flex justify-content-between p-3">
+            <div className="CompanyCard-Avatar mr-auto ml-auto">
+                <img src={companyLogo} alt={company.name} className="card-img-top rounded-circle border border-dark" />
+            </div>
+            <div className="card-text ml-auto text-left w-75">
+                <h5 className="card-title">{company.name}</h5>
+                <p className="card-text">{company.description}</p>
+            </div>
+        </div>
         </Link>
-            <h1 className="card-title" >{company.name}</h1>
-            <p className="card-text" >{company.description}</p>
         </div>
     )
 }
