197206cfe678f2b171ad0f7329d0df2eb29f5d5a
kevinreber <kevinreber1@gmail.com>
2020-07-14 20:45:11 -0700

    feat: can click on company's avatar to visit company's job posts

--- frontend/src/Components/CompanyCard.jsx
+++ frontend/src/Components/CompanyCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './CompanyCard.css';
 
 function CompanyCard({company}){
@@ -6,7 +7,9 @@ function CompanyCard({company}){
     const companyLogo = company.logo_url ? company.logo_url : defaultLogo;
     return(
         <div id={company.handle} className="CompanyCard card w-75">
+        <Link to={`/companies/${company.handle}`} >
             <img src={companyLogo} alt={company.name} className="card-img-top rounded-circle border border-dark" />
+        </Link>
             <h1 className="card-title" >{company.name}</h1>
             <p className="card-text" >{company.description}</p>
         </div>
