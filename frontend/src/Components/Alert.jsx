93a740769577c60ca89a6d968277096c7163e66b
kevinreber <kevinreber1@gmail.com>
2020-07-18 15:19:05 -0700

    refact: cleaned up code

--- frontend/src/Components/Alert.jsx
+++ frontend/src/Components/Alert.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 function Alert({ type, messages }){
 
-    const alertList = (messages.map(message => (
-      <p className="mb-0 small" key={ message }>
-        { message }
-      </p>
-    )));
+  const alertList = (messages.map(message => (
+    <p className="mb-0 small" key={ message }>
+      { message }
+    </p>
+  )));
 
-    return (
-      <div className={`alert alert-${type}`} role="alert">
+  return (
+    <div className={`alert alert-${type}`} role="alert">
       { alertList }
-      </div>
-    );
-
+    </div>
+  );
 }
 
 export default Alert;
