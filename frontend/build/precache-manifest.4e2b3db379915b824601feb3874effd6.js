17d832bd3e1f3baf6a69d94bbd530233f43e6708
kevinreber <kevinreber1@gmail.com>
2020-07-22 18:14:49 -0700

    deploy added build

---
+++ frontend/build/precache-manifest.4e2b3db379915b824601feb3874effd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e1abb589128ccb30ab791f37bf5c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82f36411f527dcb51ec5",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a2e419735b6b1fec9124",
+    "url": "/static/css/main.b0213b27.chunk.css"
+  },
+  {
+    "revision": "82f36411f527dcb51ec5",
+    "url": "/static/js/2.84faf44e.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/static/js/2.84faf44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e419735b6b1fec9124",
+    "url": "/static/js/main.f3099dae.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
